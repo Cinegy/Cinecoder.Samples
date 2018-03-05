7c7b14f342d10a4ce90aced592af4780c58e5ba4
Lewis Kirkaldie <lewis.kirkaldie@gmail.com>
2018-03-05 20:08:18 +0000

    -relabel google test project as native project -clean up native unit tests -stream message into ASSERT tests

---
+++ Daniel2.Native.Tests/CDaniel2Encoding.cpp
@@ -0,0 +1,73 @@
+#include "pch.h"
+#include "CEncoderTest.h"
+#include <Cinecoder_i.c>
+
+#define COMPANYNAME "cinegy"
+#define LICENSEKEY "R5H6F6YDRHG51CEM1SC79SN1U4ZC6T3NYB4KWS54GBFTC7KPM1TJCY4HUF5CC4NG"
+
+
+CComPtr<ICC_ClassFactory> p_factory;
+CComPtr<ICC_VideoEncoder> p_encoder;
+CComPtr<ICC_DanielVideoEncoderSettings> p_settings;
+
+TEST(Daniel2,CheckCinecoderVersion)
+{
+	const auto ver = Cinecoder_GetVersion();
+	printf("Cinecoder version %d.%02d.%02d\n", ver.VersionHi, ver.VersionLo, ver.EditionNo);
+
+}
+
+TEST(Daniel2,CreateCinecoderFactory)
+{
+	const auto hr = Cinecoder_CreateClassFactory(&p_factory);
+	ASSERT_HRESULT_SUCCEEDED(hr) << "Cinecoder factory creation error";
+}
+
+
+TEST(Daniel2,AssignCinecoderLicense)
+{
+	const auto hr = p_factory->AssignLicense(COMPANYNAME, LICENSEKEY);
+	ASSERT_HRESULT_SUCCEEDED(hr) << "AssignLicense error";
+}
+
+TEST(Daniel2,CreateEncoder)
+{
+	const auto hr = p_factory->CreateInstance(CLSID_CC_DanielVideoEncoder, IID_ICC_VideoEncoder, reinterpret_cast<IUnknown**>(&p_encoder));
+	ASSERT_HRESULT_SUCCEEDED(hr) << "Encoder creation error";
+}
+
+TEST(Daniel2,CreateSettings)
+{
+	const auto hr = p_factory->CreateInstance(CLSID_CC_DanielVideoEncoderSettings, IID_ICC_DanielVideoEncoderSettings, reinterpret_cast<IUnknown**>(&p_settings));
+	ASSERT_HRESULT_SUCCEEDED(hr) << "Encoder settings creation error";
+}
+
+TEST(Daniel2, InitEncoder)
+{	
+	p_settings->put_FrameSize(MK_SIZE(7680, 4320));
+	p_settings->put_FrameRate(MK_RATIONAL(60000, 1001));
+	p_settings->put_InputColorFormat(CCF_V210);
+	p_settings->put_ChromaFormat(CC_CHROMA_422);
+	p_settings->put_BitDepth(10);
+	p_settings->put_RateMode(CC_CBR);
+	p_settings->put_BitRate(1000000000);
+	p_settings->put_CodingMethod(CC_D2_METHOD_CUDA);
+	//p_settings->put_DeviceID(0); //CUDA settings only
+	p_settings->put_NumSingleEncoders(4);
+	
+	const auto hr = p_encoder->Init(p_settings);
+	ASSERT_HRESULT_SUCCEEDED(hr) << "Encoder initialization error";
+}
+
+TEST(Daniel2, AssignEncoderParameters)
+{		
+	ENCODER_PARAMS par = {};
+	par.pEncoder = p_encoder;
+	par.ColorFormat = CCF_V210;
+	par.NumReadThreads = 4;
+	par.QueueSize = 16;
+
+	CEncoderTest test;
+	const auto hr = test.AssignParameters(par);
+	ASSERT_HRESULT_SUCCEEDED(hr) << "EncoderTest.AssignParameters error";
+}
