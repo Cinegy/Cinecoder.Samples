08f14a18ebbea0cfae344375a14aeea3ed1a0e1a
Nick Velichko <nick@cinegy.com>
2018-06-20 18:08:47 +0300

    * updated license key (valid thru 15/07/2018)

--- cinecoder_license_string.cs
+++ cinecoder_license_string.cs
@@ -1,6 +1,6 @@
 internal static class License
 {
-    //License valid thru 15/06/2018
+    //License valid thru 15/07/2018
     internal const string Companyname = "cinegy";
-    internal const string Licensekey = "R9HYZ160XHFW6GHT2HG05HT2ASZG2NPN2BH79E5SFPFMZDK3J1MJF1HHXG8ZZEMG";
+    internal const string Licensekey = "A9H2G16DAEG82ZHNY4G75EJ2RECGYMBT63S08H9SGBGJZD0PJYJJC1SHAG9ZFSNZ";
 }
