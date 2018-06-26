a655e0715f4065ef025cecc99496efd4718d8708
Nick Velichko <nick@cinegy.com>
2018-06-27 01:22:39 +0300

    + SimpleAudioEncoder test app

---
+++ SimpleAudioEncoder/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using Cinecoder.Interop;
+
+namespace SimpleAudioEncoder
+{
+    class Program
+    {
+        static unsafe int Main(string[] args)
+        {
+            Console.WriteLine("Simple Audio Encoder Test App # 1.00. Copyright (c) 2018 Cinegy LLC\n");
+
+            if (args.Length < 3)
+            {
+                Console.WriteLine("This sample application encodes a PCM audio file into elementary coded audio file (.mp3, .aac or whatever)\n");
+                Console.WriteLine("Usage: SimpleAudioEncoder.exe {MPEG|AAC} <input_file> <output_file> <switches>");
+                Console.WriteLine("Where switches are:");
+                Console.WriteLine("  /br=#   - bitrate (Kbps)");
+                Console.WriteLine("  /nch=#  - number of channels");
+                Console.WriteLine("  /freq=# - sample rate");
+                return 1;
+            }
+
+            Console.WriteLine($"Cinecoder version: {Cinecoder_.Version.VersionHi}.{Cinecoder_.Version.VersionLo}.{Cinecoder_.Version.EditionNo}.{Cinecoder_.Version.RevisionNo}\n");
+
+            try
+            {
+                Cinecoder_.ErrorHandler = new ErrorHandler();
+
+                ICC_ClassFactory Factory = Cinecoder_.CreateClassFactory();
+                Factory.AssignLicense(License.Companyname, License.Licensekey);
+
+                int nch = 2;
+                int freq = 48000;
+                int br = 192;
+
+                for(int i = 3; i < args.Length; i++)
+                {
+                    if (args[i].ToLower().StartsWith("/br="))
+                        br = Int32.Parse(args[i].Substring(4));
+                    else if (args[i].ToLower().StartsWith("/nch="))
+                        nch = Int32.Parse(args[i].Substring(5));
+                    else if (args[i].ToLower().StartsWith("/freq="))
+                        freq = Int32.Parse(args[i].Substring(6));
+                    else
+                        throw new Exception($"Unknow argument: {args[i]}");
+                }
+
+                ICC_AudioEncoder audioEncoder = null;
+
+                if (args[0] == "MPEG")
+                {
+                    if (nch != 1 && nch != 2)
+                        throw new Exception("MPEG Audio Encoder: only 1 or 2 audio channels are supported");
+
+                    var encPar = Factory.CreateInstanceByName("MpegAudioEncoderSettings") as ICC_MpegAudioEncoderSettings;
+                    encPar.BitRate = br * 1000;
+                    encPar.ChannelMode = nch == 1 ? CC_MPG_AUDIO_CHANNEL_MODE.CC_MPG_ACH_MONO : CC_MPG_AUDIO_CHANNEL_MODE.CC_MPG_ACH_STEREO;
+                    encPar.SampleRate = (uint)freq;
+
+                    audioEncoder = Factory.CreateInstanceByName("MpegAudioEncoder") as ICC_AudioEncoder;
+                    audioEncoder.Init(encPar);
+                }
+                else if (args[0] == "AAC")
+                {
+                    var encPar = Factory.CreateInstanceByName("AAC_AudioEncoderSettings") as ICC_AAC_AudioEncoderSettings;
+                    encPar.BitRate = br * 1000;
+                    encPar.NumChannels = (uint)nch;
+                    encPar.SampleRate = (uint)freq;
+
+                    audioEncoder = Factory.CreateInstanceByName("AAC_AudioEncoder") as ICC_AudioEncoder;
+                    audioEncoder.Init(encPar);
+                }
+                else
+                    throw new Exception("$Unknown audio encoder type: {args[0]}");
+
+                BinaryReader inputFile = new BinaryReader(new FileStream(args[1], FileMode.Open));
+
+                var outputFile = Factory.CreateInstanceByName("OutputFile") as ICC_OutputFile;
+                outputFile.Create(args[2]);
+
+                audioEncoder.OutputCallback = outputFile;
+
+                var buffer = new byte[8 * 1024];
+
+                for (;;)
+                {
+                    int bytes_read = inputFile.Read(buffer, 0, buffer.Length);
+
+                    if (bytes_read == 0)
+                        break;
+
+                    fixed (byte* p = buffer)
+                        audioEncoder.ProcessAudio(CC_AUDIO_FMT.CAF_PCM16, (IntPtr)p, (uint)bytes_read);
+                }
+
+                audioEncoder.Done(true);
+                outputFile.Close();
+            }
+            catch (Exception e)
+            {
+                if (e.Message != null)
+                    Console.Error.WriteLine($"Error: {e.Message}");
+
+                return e.HResult;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ SimpleAudioEncoder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleAudioEncoder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleAudioEncoder")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bf3e204-f272-4a59-872e-3c9973b24d00")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
