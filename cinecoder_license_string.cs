b90e10bb5be8444ea9c466209f7b90dc247ea044
Nick Velichko <nick@cinegy.com>
2018-08-17 18:21:57 +0300

    -update embedded cinecoder license to run until 15th September 2018

--- cinecoder_license_string.cs
+++ cinecoder_license_string.cs
@@ -2,5 +2,5 @@ internal static class License
 {
     //License valid thru 15/08/2018
     internal const string Companyname = "cinegy";
-    internal const string Licensekey = "X84YC210XHZ9YZSTY4CKW4M6AEZC6MPNYPS7WHW4GBGMZKDBM2NJC14HUC5GC4JZ";
+    internal const string Licensekey = "U9H1F6Y7USF86ZHN6HG7WHT6U4ZCYNBT2PH05E54FLFMZDDBNYMNC6S4XZ9ZG4MF";
 }
