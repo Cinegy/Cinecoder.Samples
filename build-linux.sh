43c2b8d179bcbbfd935a5bffc574d32f429cbd22
Nick Velichko <nick@cinegy.com>
2019-02-01 18:46:36 +0300

    Fixed build-linux.sh in case where _bin dir already exists

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,7 @@
 command -v nuget >/dev/null 2>&1 || { echo >&2 "NuGet isn't found. Please install it at first."; exit 1; }
 nuget restore
 
-mkdir _build.tmp && cd "$_"
+mkdir _build.tmp
+cd "$_"
 cmake .. && make -j8
 cd ..
