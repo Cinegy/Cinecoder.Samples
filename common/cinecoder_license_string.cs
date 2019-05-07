4ccbb5490949b9b3f67180fadf93fa3b29b9567b
Lewis Kirkaldie <lewis.kirkaldie@gmail.com>
2019-05-07 10:50:03 +0100

    Delete cinecoder_license_string.cs

--- common/cinecoder_license_string.cs
+++
@@ -1,6 +0,0 @@
-internal static class License
-{
-    //License valid thru 27/02/2019
-    internal const string Companyname = "CinecoderSamples";
-    internal const string Licensekey = "22UMB72F60BL5M0C4XN345N83R3JD0SP0MEE3DHFY18FDHCDRPFKRE5B9WABS604";
-}
