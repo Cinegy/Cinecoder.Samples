75e40d5d9fd59b99af96db02aa0146c0f4d78314
Lewis Kirkaldie <lewis@gmail.com>
2019-11-27 21:55:28 +0000

    -add powershell script to create license files

---
+++ common/inject-license.ps1
@@ -0,0 +1,33 @@
+# PowerShell Cinegy Build Script
+# COPYRIGHT Cinegy GmbH 2019
+param([string]$companyName="cinegy",[string]$licenseKey="FFFFFF")
+
+#define regular expressions to be used when checking for #define statements
+$companyNameRegex = "(\s*#define\s+COMPANYNAME\s+`")(\S+)`""
+$licenseKeyRegex = "(\s*#define\s+LICENSEKEY\s+`")(\S+)`""
+
+$FileLines = Get-Content -path ".\cinecoder_license_string_example.h"  
+
+for($i=0;$i -lt $FileLines.Count;$i++)
+{
+    $FileLines[$i] = $FileLines[$i] -Replace $companyNameRegex, "`${1}$companyName`""
+    $FileLines[$i] = $FileLines[$i] -Replace $licenseKeyRegex, "`${1}$licenseKey`""
+}
+
+[Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath
+[System.IO.File]::WriteAllLines("cinecoder_license_string.h", $FileLines)
+
+
+#define regular expressions to be used when checking for const statements
+$companyNameRegex = "(\s*#define\s+COMPANYNAME\s+`")(\S+)`"" #todo: update for the actual CS values
+$licenseKeyRegex = "(\s*#define\s+LICENSEKEY\s+`")(\S+)`""
+
+$FileLines = Get-Content -path ".\cinecoder_license_string_example.cs"  
+
+for($i=0;$i -lt $FileLines.Count;$i++)
+{
+    $FileLines[$i] = $FileLines[$i] -Replace $companyNameRegex, "`${1}$companyName`""
+    $FileLines[$i] = $FileLines[$i] -Replace $licenseKeyRegex, "`${1}$licenseKey`""
+}
+
+[System.IO.File]::WriteAllLines("cinecoder_license_string.cs", $FileLines)
