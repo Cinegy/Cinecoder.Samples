2c0032fb56515634cecbb3b454879112ea4791d0
Nick Velichko <velichko@cinegy.com>
2020-06-23 20:46:25 +0300

    - added Daniel2.Benchmark project

---
+++ Daniel2.Benchmark/cpu_load_meter.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#if defined(_WIN32)
+
+#include <windows.h>
+
+class CpuLoadMeter
+{
+	unsigned long long prev_IdleTime, prev_KernelTime, prev_UserTime;
+
+public:
+	CpuLoadMeter()
+	{
+		GetSystemTimes((FILETIME*)&prev_IdleTime, (FILETIME*)&prev_KernelTime, (FILETIME*)&prev_UserTime);
+	}
+
+	float GetLoad()
+	{
+        unsigned long long IdleTime, KernelTime, UserTime;
+        GetSystemTimes((FILETIME*)&IdleTime, (FILETIME*)&KernelTime, (FILETIME*)&UserTime);
+
+        auto usr = UserTime   - prev_UserTime;
+        auto ker = KernelTime - prev_KernelTime;
+        auto idl = IdleTime   - prev_IdleTime;
+        auto sys = ker + usr;
+
+        prev_IdleTime   = IdleTime;
+        prev_KernelTime = KernelTime;
+        prev_UserTime   = UserTime;
+
+        return 100.0f * (sys - idl) / sys;
+	}
+};
+
+#elif defined(__linux__)
+
+class CpuLoadMeter
+{
+    long long prev_busy, prev_work;
+
+public:
+    CpuLoadMeter()
+    {
+        ReadProcStat(&prev_busy, &prev_work);
+    }
+
+    float GetLoad()
+    {
+        long long busy, work;
+
+        ReadProcStat(&busy, &work);
+
+        float usage = work == prev_work ? 0.0 : 100.0 * (busy - prev_busy) / (work - prev_work);
+
+        prev_work = work;
+        prev_busy = busy;
+
+        return usage;
+    }
+
+    static void ReadProcStat(long long *busy, long long *work)
+    {
+        long long dummy, cpu, nice, sys, idle;
+
+        FILE *f = fopen("/proc/stat", "rt");
+        fscanf(f, "cpu %lld %lld %lld %lld", &cpu, &nice, &sys, &idle);
+        fclose(f);
+
+        *busy = cpu + nice + sys;
+        *work = *busy + idle;
+    }
+};
+
+#else
+
+class CpuLoadMeter
+{
+public:
+    float GetLoad()
+    {
+        return -1;
+    }
+};
+
+#endif
---
+++ Daniel2.Benchmark/file_writer.h
@@ -0,0 +1,125 @@
+#include "implement_iunknown_static.h"
+#include <malloc.h>
+#include <vector>
+#include <utility>
+
+//------------------------------------------------------------------
+class C_FileWriter : public ICC_ByteStreamCallback, public ICC_DataWriterEx
+//------------------------------------------------------------------
+{
+  _IMPLEMENT_IUNKNOWN_2_STATICALLY(ICC_ByteStreamCallback, ICC_DataWriterEx)
+
+  FILE *m_File;
+
+  int m_NumFrames;
+  long long m_TotalDataSize;
+
+  bool m_bCatchFrames;
+
+  typedef std::pair<BYTE*,size_t> CodedFrameData;
+  std::vector< CodedFrameData > m_CaughtFrames;
+
+public:
+  C_FileWriter(FILE *f, bool bCatchFrames = true) : m_File(f), m_bCatchFrames(bCatchFrames)
+  {
+    m_NumFrames = 0;
+    m_TotalDataSize = 0;
+  }
+
+  //-----------------------------------------------------------------------------
+  virtual ~C_FileWriter()
+  //-----------------------------------------------------------------------------
+  {
+  }
+
+  //-----------------------------------------------------------------------------
+  STDMETHOD(ProcessData)(const BYTE *pbData, CC_AMOUNT cbSize, CC_TIME, IUnknown *pProducer)
+  //-----------------------------------------------------------------------------
+  {
+    if(m_bCatchFrames)
+    {
+	  com_ptr<ICC_VideoEncoder> pVProd;
+	  if(S_OK == pProducer->QueryInterface(IID_ICC_VideoEncoder, (void**)&pVProd))
+      {
+        com_ptr<ICC_VideoFrameInfo> pFrameInfo;
+        pVProd->GetVideoFrameInfo(&pFrameInfo);
+
+        CC_FRAME_TYPE frame_type;
+        pFrameInfo->get_FrameType(&frame_type);
+
+        if(frame_type == CC_I_FRAME && !m_CaughtFrames.empty())
+        {
+          printf("\n@@: collected %zd frame(s)\n", m_CaughtFrames.size());
+          m_bCatchFrames = false;
+        }
+        else
+        {
+          BYTE *ptr = (BYTE*)malloc(cbSize);
+          memcpy(ptr, pbData, cbSize);
+          m_CaughtFrames.push_back( std::make_pair(ptr, cbSize) );
+        }
+      }
+    }
+
+  	return Write(pbData, cbSize);
+  }
+
+  size_t GetCodedSequenceLength() const
+  {
+    return m_CaughtFrames.size();
+  }
+
+  CodedFrameData GetCodedFrame(size_t i)
+  {
+    return m_CaughtFrames[i];
+  }
+
+  //-----------------------------------------------------------------------------
+  STDMETHOD(Write)(const BYTE *pbData, CC_AMOUNT cbSize)
+  //-----------------------------------------------------------------------------
+  {
+    if(m_NumFrames == 0)
+      g_EncoderTimeFirstFrameOut = system_clock::now();
+
+  	m_TotalDataSize += cbSize;
+  	m_NumFrames++;
+  	//InterlockedCompareExchange(&g_target_bitrate, 
+
+  	if(m_File == NULL)
+  	  return S_FALSE;
+
+  	if(fwrite(pbData, 1, cbSize, m_File) != cbSize)
+  	  return E_FAIL;
+    	
+  	return S_OK;
+  }
+
+  //-----------------------------------------------------------------------------
+  STDMETHOD(WriteDirect)(long long offs, const BYTE *pbData, CC_AMOUNT cbSize)
+  //-----------------------------------------------------------------------------
+  {
+  	if(m_File == NULL)
+  	  return S_FALSE;
+
+    long long prev = _ftelli64(m_File);
+    _fseeki64(m_File, offs, SEEK_SET);
+    HRESULT hr = Write(pbData, cbSize);
+    _fseeki64(m_File, prev, SEEK_SET);
+    return hr;
+  }
+
+  //-----------------------------------------------------------------------------
+  STDMETHOD(GetCurrentOffset)(long long *ppos)
+  //-----------------------------------------------------------------------------
+  {
+  	if(m_File == NULL)
+  	  return E_ACCESSDENIED;
+
+  	if(ppos == NULL)
+  	  return E_POINTER;
+
+  	*ppos = _ftelli64(m_File);
+
+  	return S_OK;
+  }
+};
