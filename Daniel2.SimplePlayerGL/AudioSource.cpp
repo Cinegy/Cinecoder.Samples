02175dae154a8a71627c93e6a73d5c14f75936c5
shemet <shemet@cinegy.com>
2020-07-28 17:51:57 +0200

    - fix warnings

--- Daniel2.SimplePlayerGL/AudioSource.cpp
+++ Daniel2.SimplePlayerGL/AudioSource.cpp
@@ -313,9 +313,9 @@ int AudioSource::OpenFile(const char* const filename)
 		case CC_ES_TYPE_AUDIO_SMPTE302: printf("SMPTE302 / "); break;
 		}
 		if (NumChannels == 1) printf("1 channel / ");
-		else printf("%zu channels / ", NumChannels);
+		else printf("%lu channels / ", NumChannels);
 		printf("%.2f kHz / ", ((double)SampleRate / 1000.0));
-		printf("%zu bits", BitsPerSample);
+		printf("%lu bits", BitsPerSample);
 		printf("\n");
 
 		if (iCurrentAudioTrackNumber == i)
