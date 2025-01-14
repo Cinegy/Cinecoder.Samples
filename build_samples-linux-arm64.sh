8f2741722f51748aed53b8cb7ad9dbd190820d1a
Vitaliy Shemet <shemet@cinegy.com>
2025-01-14 20:15:08 +0100

    - try ti use build of UbuntuARM64

--- build_samples-linux-arm64.sh
+++ build_samples-linux-arm64.sh
@@ -7,7 +7,8 @@ set -o pipefail
 #command -v nuget >/dev/null 2>&1 || { echo >&2 "NuGet isn't found. Please install it at first."; exit 1; }
 mono /usr/local/bin/nuget.exe restore
 
-[ -d _build.tmp ] || mkdir _build.tmp
+[ -d _build.tmp ] && rm -rf _build.tmp
+mkdir _build.tmp
 cd _build.tmp
 
 BUILDTYPE=${1:-Debug}
